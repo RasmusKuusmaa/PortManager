2d095e2e88933453294c9a772cc44953bb73f3a9
77f315 <77f315@gmail.com>
2025-10-27 21:56:26 +0200

    Portmodel

---
+++ PortManager/Models/PortInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortManager.Models
+{
+    public class PortInfo
+    {
+        // always displayed
+        public int PID { get; set; } = -1;
+        public int Port { get; set; } = -1;
+        public string ProcessName { get; set; } = "N/A";
+        public string Protocol { get; set; } = "N/A";
+        public string LocalAddress { get; set; } = "N/A";
+        public string State { get; set; } = "N/A";
+
+        // details page
+        public string ExecutablePath { get; set; } = "N/A";
+        public string RemoteAddress { get; set; } = "N/A";
+        public string UserName { get; set; } = "N/A";
+        public DateTime StartTime { get; set; } = DateTime.MinValue;
+        public double MemoryMB { get; set; } = 0.0;
+    }
+}
