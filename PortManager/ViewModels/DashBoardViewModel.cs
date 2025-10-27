ac0234e590d27f3895f5996a41920625c0f4e849
77f315 <77f315@gmail.com>
2025-10-27 22:47:34 +0200

    search ports

--- PortManager/ViewModels/DashBoardViewModel.cs
+++ PortManager/ViewModels/DashBoardViewModel.cs
@@ -13,6 +13,17 @@ namespace PortManager.ViewModels
 {
     public class DashBoardViewModel : BaseViewModel
     {
+        private ObservableCollection<PortInfoMain> _allports;
+
+        public ObservableCollection<PortInfoMain> AllPorts
+        {
+            get { return _ports; }
+            set
+            {
+                _ports = value;
+                OnPropertyChanged();
+            }
+        }
         private ObservableCollection<PortInfoMain> _ports;
 
         public ObservableCollection<PortInfoMain> Ports
@@ -22,31 +33,59 @@ namespace PortManager.ViewModels
                 OnPropertyChanged();
             }
         }
+        private string _searchText;
+
+        public string SearchText
+        {
+            get { return _searchText; }
+            set { _searchText = value;
+                OnPropertyChanged();
+                FilterPorts();
+            }       
+        }
 
         public RelayCommand<int> KillCommand { get; }
 
         public DashBoardViewModel()
         {
+            _allports = new ObservableCollection<PortInfoMain>();
             Ports = new ObservableCollection<PortInfoMain>();
             KillCommand = new RelayCommand<int>(
-           pid =>
-           {
-               if (MessageBox.Show($"Kill process {pid}?", "Confirm", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+               pid =>
                {
-                   PortService.KillProcess(pid);
-                   LoadPorts();
-               }
-           },
-           pid => pid > 0 
-       );
+                   if (MessageBox.Show($"Kill process {pid}?", "Confirm", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                   {
+                       PortService.KillProcess(pid);
+                       LoadPorts();
+                   }
+               },
+               pid => pid > 0 
+           );
             LoadPorts();
         }
 
         private void LoadPorts()
         {
-            Ports.Clear();
             var fetchedPorts = PortService.FetchAllPorts();
-            foreach (var p in fetchedPorts)
+            foreach(var p in fetchedPorts)
+            {
+                _allports.Add(p);
+            }
+            FilterPorts();
+        }
+
+        private void FilterPorts()
+        {
+            Ports.Clear();
+            var filtered = string.IsNullOrWhiteSpace(SearchText)
+                ? _allports
+
+                : _allports.Where(p =>
+                    p.ProcessName.Contains(SearchText, StringComparison.OrdinalIgnoreCase) ||
+                    p.Port.ToString().Contains(SearchText) ||
+                    p.Protocol.Contains(SearchText, StringComparison.OrdinalIgnoreCase));
+
+            foreach (var p in filtered)
                 Ports.Add(p);
         }
     }
